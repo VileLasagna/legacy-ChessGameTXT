27897ff96280301864704df10229fecda3e028a0
Vile Lasagna <hayros666@gmail.com>
2017-03-24 14:04:33 +1300

    Fixed constness issues in piece

--- src/piece.h
+++ src/piece.h
@@ -6,7 +6,7 @@
  */
 
 
-#include <Vector>
+#include <vector>
 #include "Pos.h"
 #include "Square.h"
 #include "FatalErrorException.h"
@@ -27,22 +27,22 @@ public:
 
 	void kill();
 	virtual bool move (Pos to, BD& vec);
-	const bool canMove(BD& vec);//returns true if the piece can move at all. Used to check for mates.
-	const virtual bool canMove(Pos& to, BD& vec) = 0;
-	const virtual bool canGo (Pos& to, BD& vec) = 0; // canGo is like canMove, but doesn't check for leaving your king in check. This is to avoid infinite loops
-	const virtual std::vector<Pos> getMoves(BD& vec) = 0;
+	bool canMove(BD& vec) const;//returns true if the piece can move at all. Used to check for mates.
+	virtual bool canMove(Pos& to, BD& vec) const = 0;
+	virtual bool canGo (Pos& to, BD& vec) const = 0; // canGo is like canMove, but doesn't check for leaving your king in check. This is to avoid infinite loops
+	virtual std::vector<Pos> getMoves(BD& vec) const = 0;
 	void setPos(Pos& to, BD& vec);//setPos is just to use for move checking on temporary copied boards. WARNING: it alters piece::at, so be sure to revert it after testing
 	piece(); //constructor that sets firstmove (used to do s'more stuff but was pwned due to technicalities)
-	pieceID::Type getType();
-	pieceID getID();
+	pieceID::Type getType() const;
+	pieceID getID() const;
 	virtual ~piece();
 	void clearPassant(BD& vec); //removes all enPassant flags from the board. MUST be called after each successful move!!
-	bool getCheck(BD& vec); //returns true if the owner of this piece is in check. Why not board -> Check for move
-	bool getColour();
+	bool getCheck(BD& vec) const; //returns true if the owner of this piece is in check. Why not board -> Check for move
+	bool getColour() const;
 	Pos getPos();
-	const bool first();
-	int getValue();
-	void printMe();
+	bool first() const;
+	int getValue() const;
+	void printMe() const;
 	void setMoved(); // for castling purposes
 	bool isLocked(BD& vec); //this is to check Stale and checkmate. That what getmoves was for (for nor), but it's not working properly so I'm gonna do a dirty, ugly but sure way and leave getMoves to be fixed some other time.
 
